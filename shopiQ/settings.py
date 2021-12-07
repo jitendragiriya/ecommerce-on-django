53cb313f5076db887c24559a8889032f758fa6af
Jitendra <jitendragiriya@gmail.com>
2021-12-07 15:22:29 +0530

    third commit

--- shopiQ/settings.py
+++ shopiQ/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wr+v4amek5ry44_dpgpq@4*8wfdm8cnwapptrd(!ozgo$k0&yv'
+# SECRET_KEY = 'django-insecure-wr+v4amek5ry44_dpgpq@4*8wfdm8cnwapptrd(!ozgo$k0&yv'
+SECRET_KEY = ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -134,4 +134,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
